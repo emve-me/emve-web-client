01ebbb083f762b57b00263f462f3c5a5110bb112
robert <rob@collaborizm.com>
2018-10-17 15:40:17 -0400

    using google endpoint

--- lib/components/withApollo.ts
+++ lib/components/withApollo.ts
@@ -2,7 +2,7 @@ import withApollo from 'next-with-apollo'
 import { createHttpLink } from 'apollo-link-http'
 import { ApolloLink, concat } from 'apollo-link'
 import { withClientState } from 'apollo-link-state'
-import { ApolloClient } from 'apollo-client'
+import ApolloClient from 'apollo-boost'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
 import getConfig from 'next/config'
@@ -10,20 +10,7 @@ import getConfig from 'next/config'
 
 export default withApollo(
   ({ headers }) => {
-    const cache = new InMemoryCache({})
-
-
-
     const gqlEndpoint = getConfig().publicRuntimeConfig.graphQLEndpoint
-
-    const linkHttp = createHttpLink({
-      uri: gqlEndpoint,
-      credentials: 'include'
-    })
-
-    return new ApolloClient({
-      link: linkHttp,
-      cache
-    })
+    return new ApolloClient({ uri: gqlEndpoint, credentials: 'include' })
   }
 )
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,53 +9,43 @@ type props = {}
 
 type state = {}
 
-const CCCCCC = gql`query getLoggedInUser {
-    auth {
-        loggedInUser {
-            name
-            id
+
+const QQQQ = gql`
+    mutation authenticateGoogle($idToken:String!){
+        authentication{
+            google(idToken: $idToken){
+                name
+                id
+                email
+            }
         }
     }
-}
+
 `
+
+
 
 export default class Index extends React.Component<props, state> {
   render() {
 
     return <div>
-
-
-      <GoogleLogin
-        onSuccess={async response => {
-          console.log('response', response)
-
-          const { profileObj, tokenId } = response as any
-
-          console.log('token id', tokenId)
-          setCookie('googleJWT', tokenId, 365)
-
-        }}
-        onFailure={(err) => {
-          console.error((err))
-        }}
-        clientId="1066657144492-gjcrv2nk0eghepj8mma7la5tbt0n6k22.apps.googleusercontent.com"
-      />
-
-
-      <Query query={CCCCCC} ssr={false}>{
-        ({ data, loading }) => {
-          if (!loading) {
-            console.log(data)
-          }
-
-          return <div>Loggggg</div>
-        }}
-      </Query>
-
-
+      <Mutation mutation={QQQQ}>
+        {(login, { data, loading, called, error }) => (
+          <GoogleLogin
+            onSuccess={async response => {
+              console.log('response', response)
+              const { profileObj, tokenId } = response as any
+              console.log('token id', tokenId)
+              const xxxxx = await login({ variables: { idToken: tokenId } })
+              console.log('XY', xxxxx)
+            }}
+            onFailure={(err) => {
+              console.error((err))
+            }}
+            clientId="1066657144492-gjcrv2nk0eghepj8mma7la5tbt0n6k22.apps.googleusercontent.com"
+          />)}
+      </Mutation>
     </div>
-
-
   }
 }
 
