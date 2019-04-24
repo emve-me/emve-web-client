6e8763f63b17d9bb523cbbf0ce68f11f4b213bde
Robert Lancer <rob@collaborizm.com>
2019-04-24 17:38:32 -0400

    join channel

--- lib/join/JoinChannel.tsx
+++ lib/join/JoinChannel.tsx
@@ -1,5 +1,5 @@
 import { Button, TextField } from '@material-ui/core'
-import { Component } from 'react'
+import React, { Component } from 'react'
 import JoinChannelController from './JoinChannelController'
 
 type TProps = {}
@@ -7,6 +7,7 @@ type TProps = {}
 type TState = { pairingCode: string }
 
 
+// todo handle invalid code error message
 export default class JoinChannel extends Component <TProps, TState> {
 
   state = { pairingCode: '' }
@@ -16,14 +17,56 @@ export default class JoinChannel extends Component <TProps, TState> {
     const { pairingCode } = this.state
 
     return <JoinChannelController>{({ joinChannel }) =>
-      <div>
-        <div>pairing code</div>
+      <div className='root'>
+        { /*language=CSS*/}
+        <style jsx>{`
+            .root {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-direction: column;
+                height: 100%;
+            }
 
-        <TextField value={pairingCode} onChange={e => this.setState({ pairingCode: e.target.value.toUpperCase() })}/>
-        <Button
-          onClick={async () => {
-            joinChannel(pairingCode)
-          }}>Join</Button>
+            .input {
+                border: 4px dashed #666;
+                outline: none;
+                height: 100px;
+                font-size: 50px;
+                padding: 10px;
+                width: 220px;
+                text-align: center;
+            }
+
+            .joinButton {
+                cursor: pointer;
+                flex: 1;
+                padding: 12px;
+                border-radius: 4px;
+                text-align: center;
+            }
+
+            .joinButton:hover {
+                background-color: aliceblue;
+            }
+
+        `}</style>
+
+        <div style={{ fontSize: 40 }}>Player pairing code</div>
+
+        <div style={{ paddingTop: 60, paddingBottom: 120 }}>
+          <input placeholder='CODE' autoFocus={true}
+                 onKeyDown={e => e.keyCode === 13 ? joinChannel(pairingCode) : undefined}
+                 className='input' type='text' value={pairingCode}
+                 onChange={e => this.setState({ pairingCode: e.target.value.toUpperCase() })}/>
+          <div style={{ display: 'flex', justifyContent: 'center', paddingTop: 12 }}>
+
+            <div className='joinButton' style={{ visibility: pairingCode.length >= 4 ? 'visible' : 'hidden' }}
+                 onClick={() => joinChannel(pairingCode)}>
+              Join Party!
+            </div>
+          </div>
+        </div>
       </div>
     }</JoinChannelController>
   }
