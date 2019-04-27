b6e58a41c2ac76291801a90a902fac496d68f00e
Robert Lancer <rob@collaborizm.com>
2019-04-26 20:41:11 -0400

    removed hostname

--- src/server.ts
+++ src/server.ts
@@ -25,17 +25,11 @@ app.prepare().then(() => {
   })
 
   server.all('*', (req, resp, next) => {
-
-
-    console.log('hostname', req.hostname)
     if (req.hostname.startsWith('www.')) {
-
       resp.redirect(301, 'https://' + req.hostname.replace('www.', ''))
-
     } else {
       next()
     }
-
   })
 
   server.get('*', async (req, res) => {
