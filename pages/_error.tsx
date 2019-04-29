f39a16c1766c8bf9b539e63732f823ec995fc0d4
Robert Lancer <rob@collaborizm.com>
2019-04-29 13:19:21 -0400

    error handling page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,24 +1,76 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Link from 'next/link'
 
-type props = {
-  statusCode: number
-}
-
-type state = {}
-
-export default class Error extends React.Component<props, state> {
+export default class Error extends Component<{ statusCode: number }> {
   static getInitialProps({ res, err }) {
     const statusCode = res ? res.statusCode : err ? err.statusCode : null
     return { statusCode }
   }
 
+  get message() {
+    const { statusCode } = this.props
+
+    switch (statusCode) {
+      case null:
+        return `Unknown error.`
+      case 404:
+        return `Sorry, couldn't find what you're looking for.`
+      default:
+        return `An error ${statusCode} occurred.`
+    }
+  }
+
   render() {
     return (
-      <p>
-        {this.props.statusCode
-          ? `An error ${this.props.statusCode} occurred on server`
-          : 'An error occurred on client'}
-      </p>
+      <div className="root">
+        {/*language=CSS*/}
+        <style jsx>
+          {`
+            body {
+              background-color: #fafafa;
+            }
+
+            h1 {
+              text-align: center;
+              padding-bottom: 16px;
+            }
+
+            a {
+              text-decoration: underline;
+            }
+
+            .root {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              flex-direction: column;
+            }
+
+            .brokeHeart {
+              padding-top: 50px;
+              padding-bottom: 50px;
+            }
+
+            @media only screen and (max-width: 905px) {
+              .brokeHeart {
+                padding-top: 25px;
+                padding-bottom: 25px;
+              }
+            }
+          `}
+        </style>
+
+        <img
+          className="brokeHeart"
+          style={{ width: 200 }}
+          src="/static/broken_heart.svg"
+        />
+
+        <h1>{this.message}</h1>
+        <Link href="/">
+          <a>Take me home!</a>
+        </Link>
+      </div>
     )
   }
 }
