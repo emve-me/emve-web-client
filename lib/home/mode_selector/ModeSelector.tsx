804b237994e285e3d92aec295d0a75bf4c91f653
Robert Lancer <rob@collaborizm.com>
2019-04-29 22:56:33 -0400

    mobile padding on mode selector

--- lib/home/mode_selector/ModeSelector.tsx
+++ lib/home/mode_selector/ModeSelector.tsx
@@ -83,7 +83,7 @@ export default () => (
         </CreateChannelController>
       </div>
 
-      <MobileFooterNav />
+      <MobileFooterNav style={{ paddingBottom: 24 }} />
     </div>
   </Shell>
 )
