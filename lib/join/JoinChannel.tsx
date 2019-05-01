bd96282ee142c7f010852ff731851fda9baa6b57
Robert Lancer <rob@collaborizm.com>
2019-05-01 11:31:48 -0400

    could not find party error handler

--- lib/join/JoinChannel.tsx
+++ lib/join/JoinChannel.tsx
@@ -6,90 +6,109 @@ import Button from '../ui/Button'
 // todo handle invalid code error message
 export default class JoinChannel extends Component<
   {},
-  { pairingCode: string }
+  { pairingCode: string; invalidPairingCode: boolean }
 > {
-  state = { pairingCode: '' }
+  state = { pairingCode: '', invalidPairingCode: false }
 
   render() {
-    const { pairingCode } = this.state
+    const { pairingCode, invalidPairingCode } = this.state
 
     return (
       <JoinChannelController>
-        {({ joinChannel }) => (
-          <div className="root">
-            {/*language=CSS*/}
-            <style jsx>{`
-              .root {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                flex-direction: column;
-                padding-top: 50px;
-              }
+        {({ joinChannel }) => {
+          const joinChannelEvent = async () => {
+            if (!(await joinChannel(pairingCode))) {
+              this.setState({ invalidPairingCode: true })
+            }
+          }
 
-              .input {
-                border: 4px dashed #666;
-                outline: none;
-                height: 100px;
-                font-size: 50px;
-                margin: 0;
-                padding: 10px;
-                width: 220px;
-                text-align: center;
-                background-color: #fff;
-              }
-
-              .pairingCodeContainer {
-                padding-top: 50px;
-                padding-bottom: 0px;
-              }
-
-              @media only screen and (max-width: 905px) {
+          return (
+            <div className="root">
+              {/*language=CSS*/}
+              <style jsx>{`
                 .root {
-                  padding-top: 25px;
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                  flex-direction: column;
+                  padding-top: 50px;
+                }
+
+                .input {
+                  border: 4px dashed ${invalidPairingCode ? 'red' : '#666'};
+                  outline: none;
+                  height: 100px;
+                  font-size: 50px;
+                  margin: 0;
+                  padding: 10px;
+                  width: 220px;
+                  text-align: center;
+                  background-color: #fff;
                 }
 
                 .pairingCodeContainer {
                   padding-top: 25px;
+                  padding-bottom: 0px;
                 }
-              }
-            `}</style>
 
-            <h1>Player pairing code</h1>
+                @media only screen and (max-width: 905px) {
+                  .root {
+                    padding-top: 25px;
+                  }
 
-            <div className="pairingCodeContainer">
-              <input
-                placeholder="CODE"
-                autoFocus
-                onKeyDown={({ keyCode }) => {
-                  if (keyCode === 13) joinChannel(pairingCode)
-                }}
-                className="input"
-                type="text"
-                value={pairingCode}
-                onChange={e =>
-                  this.setState({ pairingCode: e.target.value.toUpperCase() })
+                  .pairingCodeContainer {
+                    padding-top: 25px;
+                  }
                 }
-              />
-              <div
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  paddingTop: 12
-                }}>
-                <Button
-                  style={{
-                    display: pairingCode.length >= 4 ? 'block' : 'none',
-                    flex: 1
+              `}</style>
+
+              <h1>Player pairing code</h1>
+
+              {invalidPairingCode ? (
+                <div style={{ paddingTop: 16 }}>
+                  Sorry couldn't find that party, try again.
+                </div>
+              ) : (
+                false
+              )}
+
+              <div className="pairingCodeContainer">
+                <input
+                  placeholder="CODE"
+                  autoFocus
+                  onKeyDown={({ keyCode }) => {
+                    if (keyCode === 13) joinChannelEvent()
                   }}
-                  onClick={() => joinChannel(pairingCode)}>
-                  Join Party!
-                </Button>
+                  className="input"
+                  type="text"
+                  value={pairingCode}
+                  onChange={e =>
+                    this.setState({
+                      invalidPairingCode: false,
+                      pairingCode: e.target.value.toUpperCase()
+                    })
+                  }
+                />
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    paddingTop: 12
+                  }}>
+                  <Button
+                    style={{
+                      display: pairingCode.length >= 4 ? 'block' : 'none',
+                      flex: 1
+                    }}
+                    onClick={joinChannelEvent}>
+                    Join Party!
+                  </Button>
+                </div>
               </div>
+              <MobileFooterNav style={{ paddingTop: 16 }} />
             </div>
-            <MobileFooterNav style={{ paddingTop: 16 }} />
-          </div>
-        )}
+          )
+        }}
       </JoinChannelController>
     )
   }
--- lib/join/JoinChannelController.tsx
+++ lib/join/JoinChannelController.tsx
@@ -4,33 +4,43 @@ import gql from 'graphql-tag'
 import { JoinChannel as GQJoinChannel } from '../../gql_types/JoinChannel'
 import Router from 'next/router'
 
-const JOIN_CHANNEL = gql`mutation JoinChannel($channel:ID!){
-  channelJoin(input: {channelId: $channel}){
-    id
+const JOIN_CHANNEL = gql`
+  mutation JoinChannel($channel: ID!) {
+    channelJoin(input: { channelId: $channel }) {
+      id
+    }
   }
-}`
+`
 
 type TRenderProps = {
-  joinChannel: (code: string) => Promise<void>
+  joinChannel: (code: string) => Promise<boolean>
 }
 
 type TProps = {
   children: ({ joinChannel }: TRenderProps) => React.ReactNode
 }
 
-class JoinChannelController extends Component <WithApolloClient<TProps>> {
-
+class JoinChannelController extends Component<WithApolloClient<TProps>> {
   render() {
-
     const { children, client } = this.props
 
     return children({
-      joinChannel: async (code) => {
+      joinChannel: async code => {
+        if (!/^[A-Z]+$/.test(code)) {
+          return false
+        }
+
         const mutationFdx = await client.mutate<GQJoinChannel>({
           mutation: JOIN_CHANNEL,
           variables: { channel: code }
         })
-        Router.push({ pathname: '/remote', query: { p: code } })
+
+        if (mutationFdx.data.channelJoin) {
+          Router.push({ pathname: '/remote', query: { p: code } })
+          return true
+        } else {
+          return false
+        }
       }
     })
   }
