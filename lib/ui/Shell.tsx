8ee8d81c76bf69eed982f6d7ab05c2c30d4ee7a2
Robert Lancer <robert.lancer@gmail.com>
2019-12-11 14:04:52 -0500

    functional conversion going

--- lib/ui/Shell.tsx
+++ lib/ui/Shell.tsx
@@ -7,79 +7,76 @@ type TProps = {
   header?: React.ReactNode
   visibleOnMobile?: boolean
   logoVisibleOnMobile?: boolean
+  children: React.ReactNode
 }
 
-export default class Shell extends Component<TProps> {
-  static defaultProps = {
-    visibleOnMobile: true,
-    logoVisibleOnMobile: true
-  }
+export default ({
+  header,
+  visibleOnMobile = true,
+  logoVisibleOnMobile = true,
+  children
+}: TProps) => {
+  return (
+    <>
+      {/*language=CSS*/}
+      <style jsx>
+        {`
+          header {
+            border-bottom: solid 1px rgba(0, 0, 0, 0.25);
+            background-color: white;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0px 14px;
+            position: fixed;
+            top: 0;
+            right: 0;
+            left: 0;
+            height: 70px;
+          }
 
-  render() {
-    const { visibleOnMobile, logoVisibleOnMobile } = this.props
+          .bodyContainer {
+            padding-top: 70px;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+          }
 
-    return (
-      <>
-        {/*language=CSS*/}
-        <style jsx>
-          {`
+          .logo {
+            display: block;
+          }
+
+          @media only screen and (max-width: 905px) {
             header {
-              border-bottom: solid 1px rgba(0, 0, 0, 0.25);
-              background-color: white;
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-              padding: 0px 14px;
-              position: fixed;
-              top: 0;
-              right: 0;
-              left: 0;
-              height: 70px;
-            }
-
-            .bodyContainer {
-              padding-top: 70px;
-              flex: 1;
-              display: flex;
-              flex-direction: column;
+              display: ${visibleOnMobile ? 'flex' : 'none'};
+              height: 60px;
             }
 
             .logo {
-              display: block;
+              display: ${logoVisibleOnMobile ? 'block' : 'none'};
             }
 
-            @media only screen and (max-width: 905px) {
-              header {
-                display: ${visibleOnMobile ? 'flex' : 'none'};
-                height: 60px;
-              }
-
-              .logo {
-                display: ${logoVisibleOnMobile ? 'block' : 'none'};
-              }
-
-              .bodyContainer {
-                padding-top: ${visibleOnMobile ? '60px' : '0'};
-              }
+            .bodyContainer {
+              padding-top: ${visibleOnMobile ? '60px' : '0'};
             }
-          `}
-        </style>
+          }
+        `}
+      </style>
 
-        <header>
-          <Link href="/">
-            <div className="logo">
-              <HeartIcon style={{ cursor: 'pointer' }} size={30} />
-            </div>
-          </Link>
-
-          <div style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
-            {this.props.header}
+      <header>
+        <Link href="/">
+          <div className="logo">
+            <HeartIcon style={{ cursor: 'pointer' }} size={30} />
           </div>
+        </Link>
 
-          <AccountThumb thumbSize={33} />
-        </header>
-        <div className="bodyContainer">{this.props.children}</div>
-      </>
-    )
-  }
+        <div style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
+          {header}
+        </div>
+
+        <AccountThumb thumbSize={33} />
+      </header>
+      <div className="bodyContainer">{children}</div>
+    </>
+  )
 }
