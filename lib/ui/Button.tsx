566418059bea2d259edc1d3169058869a50b0d71
Robert Lancer <robert.lancer@gmail.com>
2019-12-25 00:55:36 -0500

    refactored button to a FC

--- lib/ui/Button.tsx
+++ lib/ui/Button.tsx
@@ -2,33 +2,32 @@ import React, { Component } from 'react'
 import Shell from './Shell'
 import { accentColor } from '../style/colors'
 
-export default class Button extends Component<{
+const Button: React.FC<{
   style?: React.CSSProperties
   onClick?: () => void
-}> {
-  render() {
-    const { style, onClick, children } = this.props
-    return (
-      <div className="button" style={style} onClick={onClick}>
-        {/*language=CSS*/}
-        <style jsx>{`
-          .button {
-            display: inline-block;
-            cursor: pointer;
-            padding: 10px;
-            font-size: 20px;
-            border-radius: 6px;
-            text-align: center;
-            border: 3px solid #555;
-          }
+}> = ({ style, onClick, children }) => {
+  return (
+    <div className="button" style={style} onClick={onClick}>
+      {/*language=CSS*/}
+      <style jsx>{`
+        .button {
+          display: inline-block;
+          cursor: pointer;
+          padding: 10px;
+          font-size: 20px;
+          border-radius: 6px;
+          text-align: center;
+          border: 3px solid #555;
+        }
 
-          .button:hover {
-            background-color: ${accentColor};
-            color: #fff;
-          }
-        `}</style>
-        {children}
-      </div>
-    )
-  }
+        .button:hover {
+          background-color: ${accentColor};
+          color: #fff;
+        }
+      `}</style>
+      {children}
+    </div>
+  )
 }
+
+export default Button
