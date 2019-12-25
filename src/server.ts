3a61f296f7a11b7b4372d98a5bc4b59ce552e4b6
Robert Lancer <robert.lancer@gmail.com>
2019-12-25 15:33:09 -0500

    minor formating

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import next from 'next'
 import { getCookie } from 'vanilla-cookies'
 
 const dev = process.env.NODE_ENV !== 'production'
-
 const port = process.env.PORT
+
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
